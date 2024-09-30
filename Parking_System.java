8bd0e6c59248c1f5ea11612016a8bf2054b3441f
Harsh <harshmeshram@gmail.com>
2024-09-30 10:12:34 +0530

    adding Pack Car method in Program

--- Parking_System.java
+++ Parking_System.java
@@ -10,7 +10,48 @@ public class Parking_System{
         System.out.println("Enter  the total number of PARKING SLOTS : ");
         Total_Slots=input.nextInt();
         Available_Slots=Total_Slots;
-        System.out.println(" end the running "+Available_Slots);
-        System.out.println(" end the running "+Total_Slots);
+
+        while(true){
+            System.out.println(" What would you like to do ?");
+            System.out.println("1. Park a Car ");
+            System.out.println("2. Remove a Car ");
+            System.out.println("2. View Parked Cars ");
+            System.out.println("4. Exit ");
+            
+            int Choice=input.nextInt();
+
+            switch( Choice){
+                case 1:
+
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                default:
+                    System.out.println(" Invalid Choice . please try again!")
+            }
+        }
+        
+    }
+    public static void ParkCar(){
+        if(Available_Slots==0){
+            System.out.println(" Sorry ! there are no available Parking Slots.");
+            return;
+        }
+        Scanner user=Scanner(System.in);
+
+        System.out.println(" Enter  the License plate number of the Car :")
+        String License_Plate=user.nextLine();
+
+        Parke_Car.add(License_Plate);
+        Available_Slots--;
+        
+        System.out.println(" Car Parked Sucessfully . Available Slots :"+Available_Slots);
     }
 }
